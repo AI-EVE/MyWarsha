7d780c0a53a27034f2105b58d4f604a5a6741753
Hussam <hussampc50@gmail.com>
2024-09-20 13:13:36 -0700

    Adding Date Column to Service model

--- MyWarsha_Models/Models/Service.cs
+++ MyWarsha_Models/Models/Service.cs
@@ -6,7 +6,7 @@ namespace MyWarsha_Models.Models
     {
         public int Id { get; set; }
 
-        DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
+        public DateOnly Date { get; set; } = DateOnly.FromDateTime(DateTime.Now);
 
         [ForeignKey("ClientId")]
         public Client Client { get; set; } = null!;
